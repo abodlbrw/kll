d4056a1cfa94da3a1161b7cd0cfb42d356ee2a4a
abodlbrw <133431093+abodlbrw@users.noreply.github.com>
2023-06-26 12:07:31 +0200

    Add files via upload

---
+++ kll.py
@@ -0,0 +1,168 @@
+import requests,sys,os,time
+MSA = 'max'
+pss=input("\033[1;37m [~]\033[1;35m𝗘𝗻𝘁𝗲𝗿 𝗽𝗮𝘀𝘀𝘄𝗼𝗿𝗱 :\033[1;33m")
+
+import webbrowser 
+webbrowser.open("https://t.me/rushk_3")
+
+if pss ==MSA:
+ pass
+ print("\033[1;32m          𝗬𝗼𝘂 𝗮𝗿𝗲 𝗹𝗼𝗴𝗴𝗲𝗱 𝗶𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆")
+ time.sleep(1)
+ os.system('clear')
+else:
+ exit('\033[1;31m             𝘄𝗿𝗼𝗻𝗴 𝗽𝗮𝘀𝘀𝘄𝗼𝗿𝗱 ')
+ 
+
+
+import pyfiglet
+  import pyfiglet
+
+# = = = = = = = = = = = = 
+
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;34m'#ازرق
+C = '\033[2;35m' #وردي
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح
+
+# = = = = = = = = = = = =
+
+logo = pyfiglet.figlet_format(' * MAX *')
+print(B+logo)
+import os
+try:
+    import uuid
+except:
+    os.system('pip install uuid')
+
+try:
+    from random import *
+except:
+    os.systeam('pip install random ')
+
+try:
+    import string
+except:
+    os.system('pip install string')
+
+try:
+    import requests
+except:
+    os.system('pip install requests ')
+
+try:
+    from user_agent import generate_user_agent
+except:
+    os.system('pip install user_agent ')
+
+try:
+    from datetime import datetime
+except:
+    os.system('pip install datetime ')
+
+try:
+    import time
+except:
+    os.system('pip install time')
+else:
+    os.system('clear')
+    try:
+        import pyfiglet
+    except:
+        os.system('pip install pyfiglet')
+    else:
+        os.system('clear')
+        import pyfiglet, os,sys
+        from time import sleep
+        import webbrowser
+        import random, uuid, requests, string
+        from user_agent import generate_user_agent
+        from datetime import datetime
+        from random import *
+        from time import sleep
+        import requests, os, random, json, threading, secrets, uuid
+        from colorama import Fore, Style
+        from time import sleep
+        from time import sleep
+        from datetime import datetime
+        from secrets import token_hex
+        from uuid import uuid4
+        from user_agent import generate_user_agent
+        from uuid import uuid4
+        aa = 0
+        zz = 0
+webbrowser.open('https://t.me/@rushk_3')
+B="\033[1;30m" # Black
+R="\033[1;31m" # Red
+G="\033[1;32m" # Green
+Y="\033[1;33m" # Yellow
+Bl="\033[1;34m" # Blue
+P="\033[1;35m" # Purple
+C="\033[1;36m" # Cyan
+W="\033[1;37m" # White
+def to(s):
+	for to in s + '\n':
+		sys.stdout.write(to)
+		sys.stdout.flush()
+		sleep(30. / 700)
+aa = 0
+zz = 0
+E = '\x1b[1;31m'
+G = '\x1b[1;32m'
+S = '\x1b[1;33m'
+to (f'''
+{Y} ######     ###    ##    ## ######## #### ##    ##  #######  ##     ## 
+##    ##   ## ##   ###   ##    ##     ##   ##  ##  ##     ## ##     ## 
+##        ##   ##  ####  ##    ##     ##    ####   ##     ## ##     ## 
+ ######  ##     ## ## ## ##    ##     ##     ##    ##     ## ##     ## 
+      ## ######### ##  ####    ##     ##     ##    ##     ## ##     ## 
+##    ## ##     ## ##   ###    ##     ##     ##    ##     ## ##     ## 
+ ######  ##     ## ##    ##    ##    ####    ##     #######   #######  
+''')
+import requests, time, os
+r = requests.session()
+time.sleep(3)
+ID = input(R+ '[' + G + '!' + R  + ']' + Y +'  ID :  '+W)
+sleep(1)
+print ('\n')
+token = input(R + '[' + G+ '!' + R + ']' + Y +'  ToKen : '+W)
+print ('\n')
+name = input (R + '[' + G+ '!' + R + ']' + Y +'ادخل الاسم  اللذي تريد تخمن عليه : '+W)
+print ('\n')
+user = '0987654321'
+def check():
+	while True :
+		us = str(''.join((random.choice(user) for i in range(4))))
+		email = (name + us + '@gmail.com')
+		time.sleep(5)
+		url = 'https://api16-normal-c-alisg.tiktokv.com/passport/email/send_code/?residence=AE&device_id=6923575826752275974&os_version=14.3&app_id=1233&iid=6951746276598204161&app_name=musical_ly&pass-route=1&vendor_id=EF3C1478-2AFC-4B8E-8030-C608120AECF9&locale=ar&pass-region=1&ac=WIFI&sys_region=US&ssmix=a&version_code=17.2.0&vid=EF3C1478-2AFC-4B8E-8030-C608120AECF9&channel=App%20Store&op_region=AE&os_api=18&idfa=00000000-0000-0000-0000-000000000000&install_id=6951746276598204161&idfv=EF3C1478-2AFC-4B8E-8030-C608120AECF9&device_platform=iphone&device_type=iPhone9%2C4&openudid=3ce553bec09070081e5a698d3a14a988f3642ac4&account_region=&tz_name=Asia%2FDubai&tz_offset=14400&app_language=ar&carrier_region=AE&current_region=AE&aid=1233&mcc_mnc=42402&screen_width=1242&uoo=1&content_language=&language=ar&cdid=FBF67CFE-39E1-4556-A3EB-624A20A434E1&build_number=172025&app_version=17.2.0&resolution=1242%2A2208'
+		headers = {'Host':'api16-normal-c-alisg.tiktokv.com', 
+	             'Connection':'close', 
+	             'Content-Length':'76', 
+	             'Cookie':'sessionid=b0b2ed352b9394eefc29754dfe80926c', 
+	             'x-Tt-Token':'2c593820065f9a47b9bf51281eda9604-1.0.0', 
+	             'Content-Type':'application/x-www-form-urlencoded', 
+	             'x-tt-passport-csrf-token':'b0b2ed352b9394eefc29754dfe80926c', 
+	             'sdk-version':'2', 
+	             'passport-sdk-version':'5.12.1'}
+		data = {'account_sdk_source':'app', 
+	             'email':email, 
+	             'mix_mode':'1', 
+	             'type':'31'}
+		r1 = r.post(url, data=data, headers=headers).text
+		if '"message":"success"' in r1:
+			print(R + '[' + G+ '!' + R + ']' + W + 'مربوط ' +email)
+			tlg = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text= n\\ Emil : {email}\n- me : @rushk_3 -M- MAX"
+			i = requests.post(tlg)
+			with open('tiktokemail.txt', 'a') as (x):
+				x.write(f"{email}\n")
+		elif '"description":"غير مسجل بعد"' in r1:
+			print(R + '[' + G+ '!' + R + ']' + W + 'غير مربوط ' + email)
+		else:
+			print(R + '[' + G+ '!' + R + ']' + W + 'غير مربوط ' + email)
+	
+check()
